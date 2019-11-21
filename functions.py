bc19aed51c5b01369d40f6df001b6d65ad8400c1
Emira_Harrioui <harrioui.e@gmail.com>
2019-11-21 15:41:48 +0100

    add function choose_word

--- functions.py
+++ functions.py
@@ -1,3 +1,5 @@
+import random
+from donnees import liste_mots
 
 def insert_name():
     name = input("Entrez votre nom:")
@@ -5,3 +7,10 @@ def insert_name():
         print("Vous n'avez pas saisi votre nom")
         name = input("Entrez votre nom:")
 insert_name()
+
+def choose_word():
+
+# lecture from objects contained in file
+    word_pc = random.choice(liste_mots)
+    print(word_pc)
+choose_word()
